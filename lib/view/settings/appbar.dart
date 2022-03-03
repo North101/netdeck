6a0c30a6bedf300aad6514639dba3ed71c2af4a4
Alexander Wilde <alexander.wilde87@gmail.com>
2022-03-03 14:42:09 +0000

    Removing back button in settings page

--- lib/view/settings/appbar.dart
+++ lib/view/settings/appbar.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class SettingsAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const SettingsAppBar({Key? key}) : super(key: key);
+  const SettingsAppBar({
+    this.automaticallyImplyLeading = true,
+    Key? key,
+  }) : super(key: key);
+
+  final bool automaticallyImplyLeading;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(title: const Text('Settings'));
+    return AppBar(
+      automaticallyImplyLeading: automaticallyImplyLeading,
+      title: const Text('Settings'),
+    );
   }
 }
